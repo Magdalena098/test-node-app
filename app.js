793602662d1fd9dd89689f7f7556b94cb7251df6
Magdalena <magdalena@CCPs-MacBook-Pro.local>
2017-05-22 22:21:54 +0100

    final commit

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
+// dependencies
 var express = require("express");
-// var cors = require("cors");
-
+var cors = require("cors");
 var app = express();
 var request = require('request'); 
+var path = require('path');
+var http = require('http');
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+app.use(app.router);
 
+//index route
 app.get('/', function(req, res) {res.render('index')});
-
+//second route
 app.get('/searching', function(req, res){
-  
-    const name = req.query.city;
 
-  request('https://data.gov.uk/data/api/service/health/clinics?city=' + name, function(err, body){
-      res.send(body);
-  });
+  var val = req.query.city;
+
+  var url = 'https://data.gov.uk/data/api/service/health/clinics?city=' + val;
+  console.log(url);
+
+request(url, function(err, resp, body) {
+    body = JSON.parse(body);
+	    city = body.result[0].city;
+        postcode = body.result[0].postcode;
+        name = body.result[0].name;
+        organisation = body.result[0].organisation_id;
+	    results = city + " " + postcode + " " + name + " " + organisation;
+	    // pass back the results to client side	
+       res.send(results);
+    });
 });
-
 
 
 app.listen(3000);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,11 @@
 $(function(){
-     var parameters = { search: $(this).val() };
+	console.log(1111);
+ $('#city').on('input', function(e){
+   	console.log("where is our data");
+     var parameters = { city: $(this).val() };
        $.get( '/searching',parameters, function(data) {
        $('#results').html(data);
+       console.log(data);
      });
+ });
 });
